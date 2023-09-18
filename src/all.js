7ade411cf7b1c8e4a8540784ee9ceb4047cc01c7
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-18 16:49:39 -0300

    Update all.js

--- src/all.js
+++ src/all.js
@@ -45,99 +45,108 @@ fs.readdir(directoryPath, function (err, files) {
   files.forEach(function (file) {
     const filePath = path.join(directoryPath, file);
     if (path.extname(file) === '.xlsx') {
-      const workbook = XLSX.readFile(filePath);
-      const sheet = workbook.Sheets[sheetName];
+      const match = file.match(/^(.*?) - (.*?) -/);
+      if (match) {
+        const state = match[1];
+        const thematicArea = match[2];
 
-      const idPlanGenerate = uuidv4();
+        const workbook = XLSX.readFile(filePath);
+        const sheet = workbook.Sheets[sheetName];
 
-      const resultEntry = {
-        "planId": idPlanGenerate,
-        "year": sheet['M1'] ? sheet['M1'].v : null,
-        "state": sheet['N1'] ? sheet['N1'].v : null,
-        "thematicArea": sheet['A2'] ? sheet['A2'].v : null,
-        "diagnosis": sheet['B19'] ? sheet['B19'].v : null,
-        "justification": sheet['B20'] ? sheet['B20'].v : null,
-        "generalGoal": sheet['B21'] ? sheet['B21'].v : null,
-        "implementationStrategy": sheet['B23'] ? sheet['B23'].v : null,
-        "diagnosticImplementationStrategy": sheet['B25'] ? sheet['B25'].v : null,
-        "governanceImplementationStrategy": sheet['B27'] ? sheet['B27'].v : null,
-        "capacityImplementationStrategy": sheet['B29'] ? sheet['B29'].v : null,
-        "acquisitionImplementationStrategy": sheet['B31'] ? sheet['B31'].v : null,
-        "specificGoals": []
-      };
+        const idPlanGenerate = uuidv4();
 
-      const mergedCellRange = 'B33:D50';
-      const { s, e } = XLSX.utils.decode_range(mergedCellRange);
-      let currentNumber;
+        const resultEntry = {
+          "planId": idPlanGenerate,
+          "year": sheet['M1'] ? sheet['M1'].v : null,
+          "state": state,
+          "thematicArea": thematicArea,
+          "diagnosis": sheet['B19'] ? sheet['B19'].v : null,
+          "justification": sheet['B20'] ? sheet['B20'].v : null,
+          "generalGoal": sheet['B21'] ? sheet['B21'].v : null,
+          "implementationStrategy": sheet['B23'] ? sheet['B23'].v : null,
+          "diagnosticImplementationStrategy": sheet['B25'] ? sheet['B25'].v : null,
+          "governanceImplementationStrategy": sheet['B27'] ? sheet['B27'].v : null,
+          "capacityImplementationStrategy": sheet['B29'] ? sheet['B29'].v : null,
+          "acquisitionImplementationStrategy": sheet['B31'] ? sheet['B31'].v : null,
+          "resultIndicator": sheet['B32'] ? sheet['B32'].v : null,
+          "specificGoals": []
+        };
 
-      for (let rowIndex = s.r; rowIndex <= e.r; rowIndex++) {
-        for (let colIndex = s.c; colIndex <= e.c; colIndex++) {
-          const cellAddress = XLSX.utils.encode_cell({ r: rowIndex, c: colIndex });
-          const cell = sheet[cellAddress];
-          const goalIdGenerate = uuidv4();
-          if (colIndex === s.c) {
-            currentNumber = cell ? cell.v : null;
-          } else if (cell && cell.v !== undefined && currentNumber !== null) {
-            const entry = {
-              goalId: goalIdGenerate,
-              planId: idPlanGenerate,
-              short_name: currentNumber,
-              name: cell.v,
-              actions: []
-            };
-            resultEntry["specificGoals"].push(entry);
+        const mergedCellRange = 'B33:D50';
+        const { s, e } = XLSX.utils.decode_range(mergedCellRange);
+        let currentNumber;
+
+        for (let rowIndex = s.r; rowIndex <= e.r; rowIndex++) {
+          for (let colIndex = s.c; colIndex <= e.c; colIndex++) {
+            const cellAddress = XLSX.utils.encode_cell({ r: rowIndex, c: colIndex });
+            const cell = sheet[cellAddress];
+            const goalIdGenerate = uuidv4();
+            if (colIndex === s.c) {
+              currentNumber = cell ? cell.v : null;
+            } else if (cell && cell.v !== undefined && currentNumber !== null) {
+              const entry = {
+                goalId: goalIdGenerate,
+                planId: idPlanGenerate,
+                short_name: currentNumber,
+                name: cell.v,
+                actions: []
+              };
+              resultEntry["specificGoals"].push(entry);
+            }
           }
         }
-      }
 
-      allResults.push(resultEntry);
+        allResults.push(resultEntry);
 
-      for (let rowIndex = 55; rowIndex <= 1000; rowIndex++) {
-        if (isRowEmpty(sheet, rowIndex)) continue;
+        for (let rowIndex = 55; rowIndex <= 1000; rowIndex++) {
+          if (isRowEmpty(sheet, rowIndex)) continue;
 
-        const currentMetaNum = sheet[startingCells["metaNum"] + rowIndex].v;
-        const currentActionNum = sheet[startingCells["actionNum"] + rowIndex].v;
-        const currentActionName = sheet[startingCells["adherenceArt5"] + rowIndex].v;
-        const existingMeta = resultEntry["specificGoals"].find(meta => meta.short_name === currentMetaNum);
+          const currentMetaNum = sheet[startingCells["metaNum"] + rowIndex].v;
+          const currentActionNum = sheet[startingCells["actionNum"] + rowIndex].v;
+          const currentActionName = sheet[startingCells["adherenceArt5"] + rowIndex].v;
+          const existingMeta = resultEntry["specificGoals"].find(meta => meta.short_name === currentMetaNum);
 
-        let goalIdGenerate = uuidv4();
+          let goalIdGenerate = uuidv4();
 
-        if (existingMeta) {
-          let actionObj = existingMeta["actions"].find(action => action["actionNum"] === currentActionNum);
-          if (!actionObj) {
-            const actionIdGenerate = uuidv4();
-            actionObj = {
-              actionId: actionIdGenerate,
-              goalId: goalIdGenerate,
-              actionNum: currentActionNum,
-              actionName: currentActionName,
-              items: []
+          if (existingMeta) {
+            let actionObj = existingMeta["actions"].find(action => action["actionNum"] === currentActionNum);
+            if (!actionObj) {
+              const actionIdGenerate = uuidv4();
+              actionObj = {
+                actionId: actionIdGenerate,
+                goalId: goalIdGenerate,
+                actionNum: currentActionNum,
+                actionName: currentActionName,
+                items: []
+              };
+              existingMeta["actions"].push(actionObj);
+            }
+
+            const itemData = {
+              itemId: uuidv4(),
+              actionId: actionObj.actionId,
+              financedItem: sheet[startingCells["financedItem"] + rowIndex] ? sheet[startingCells["financedItem"] + rowIndex].v : null,
+              itemDescription: sheet[startingCells["itemDescription"] + rowIndex] ? sheet[startingCells["itemDescription"] + rowIndex].v : null,
+              senaspItemCode: sheet[startingCells["senaspItemCode"] + rowIndex] ? sheet[startingCells["senaspItemCode"] + rowIndex].v : null,
+              institution: sheet[startingCells["institution"] + rowIndex] ? sheet[startingCells["institution"] + rowIndex].v : null,
+              expenseType: sheet[startingCells["expenseType"] + rowIndex] ? sheet[startingCells["expenseType"] + rowIndex].v : null,
+              plannedQuantity: sheet[startingCells["plannedQuantity"] + rowIndex] ? sheet[startingCells["plannedQuantity"] + rowIndex].v : null,
+              measurementUnit: sheet[startingCells["measurementUnit"] + rowIndex] ? sheet[startingCells["measurementUnit"] + rowIndex].v : null,
+              plannedValue: sheet[startingCells["plannedValue"] + rowIndex] ? sheet[startingCells["plannedValue"] + rowIndex].v : null,
+              processIndicatorDescription: sheet[startingCells["processIndicatorDescription"] + rowIndex] ? sheet[startingCells["processIndicatorDescription"] + rowIndex].v : null,
+              processIndicatorFormula: sheet[startingCells["processIndicatorFormula"] + rowIndex] ? sheet[startingCells["processIndicatorFormula"] + rowIndex].v : null,
+              periodicity: sheet[startingCells["periodicity"] + rowIndex] ? sheet[startingCells["periodicity"] + rowIndex].v : null,
+              pmspGoal: sheet[startingCells["pmspGoal"] + rowIndex] ? sheet[startingCells["pmspGoal"] + rowIndex].v : null,
+              pespGoal: sheet[startingCells["pespGoal"] + rowIndex] ? sheet[startingCells["pespGoal"] + rowIndex].v : null,
             };
-            existingMeta["actions"].push(actionObj);
+            for (let key in itemData) {
+              itemData[key] = typeof itemData[key] === "string" ? itemData[key].replace(/\s+/g, ' ').trim() : itemData[key];
+            }
+            actionObj["items"].push(itemData);
           }
-
-          const itemData = {
-            itemId: uuidv4(),
-            actionId: actionObj.actionId,
-            financedItem: sheet[startingCells["financedItem"] + rowIndex] ? sheet[startingCells["financedItem"] + rowIndex].v : null,
-            itemDescription: sheet[startingCells["itemDescription"] + rowIndex] ? sheet[startingCells["itemDescription"] + rowIndex].v : null,
-            senaspItemCode: sheet[startingCells["senaspItemCode"] + rowIndex] ? sheet[startingCells["senaspItemCode"] + rowIndex].v : null,
-            institution: sheet[startingCells["institution"] + rowIndex] ? sheet[startingCells["institution"] + rowIndex].v : null,
-            expenseType: sheet[startingCells["expenseType"] + rowIndex] ? sheet[startingCells["expenseType"] + rowIndex].v : null,
-            plannedQuantity: sheet[startingCells["plannedQuantity"] + rowIndex] ? sheet[startingCells["plannedQuantity"] + rowIndex].v : null,
-            measurementUnit: sheet[startingCells["measurementUnit"] + rowIndex] ? sheet[startingCells["measurementUnit"] + rowIndex].v : null,
-            plannedValue: sheet[startingCells["plannedValue"] + rowIndex] ? sheet[startingCells["plannedValue"] + rowIndex].v : null,
-            processIndicatorDescription: sheet[startingCells["processIndicatorDescription"] + rowIndex] ? sheet[startingCells["processIndicatorDescription"] + rowIndex].v : null,
-            processIndicatorFormula: sheet[startingCells["processIndicatorFormula"] + rowIndex] ? sheet[startingCells["processIndicatorFormula"] + rowIndex].v : null,
-            periodicity: sheet[startingCells["periodicity"] + rowIndex] ? sheet[startingCells["periodicity"] + rowIndex].v : null,
-            pmspGoal: sheet[startingCells["pmspGoal"] + rowIndex] ? sheet[startingCells["pmspGoal"] + rowIndex].v : null,
-            pespGoal: sheet[startingCells["pespGoal"] + rowIndex] ? sheet[startingCells["pespGoal"] + rowIndex].v : null,
-          };
-          for (let key in itemData) {
-            itemData[key] = typeof itemData[key] === "string" ? itemData[key].replace(/\s+/g, ' ').trim() : itemData[key];
-          }
-          actionObj["items"].push(itemData);
         }
+      } else {
+        console.error('Nome de arquivo não corresponde ao padrão esperado:', file);
       }
     }
   });
