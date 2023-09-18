4b605fed9009306c486c0ea3b0d263e5d643a434
Thiago Gontijo <tjgontijo@gmail.com>
2023-09-17 21:53:51 -0300

    Create delet-db.js

---
+++ src/delet-db.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+async function resetDatabase() {
+  const prisma = new PrismaClient();
+
+  try {
+    // Excluir todos os registros em ordem reversa para evitar erros de restrição de chave estrangeira
+    await prisma.item.deleteMany({});
+    await prisma.action.deleteMany({});
+    await prisma.specificGoal.deleteMany({});
+    await prisma.plan.deleteMany({});
+
+    console.log('Banco de dados zerado com sucesso.');
+  } catch (error) {
+    console.error('Erro ao zerar o banco de dados:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+resetDatabase();
